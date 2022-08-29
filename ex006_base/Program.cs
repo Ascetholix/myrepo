1887799e8fe41231e1ac704737aadb7d8469bd3f
Ascetholix <sangak@mail.ru>
2022-08-30 00:18:50 +0300

    add ex006 base

---
+++ ex006_base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 1;
+int c = 4;
+int d = 7;
+int e = 9;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.WriteLine(max);
