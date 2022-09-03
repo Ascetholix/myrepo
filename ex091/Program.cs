8c2dd446440f9519d7b0df4c0a5afd6ad9334c26
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-03 12:07:16 +0300

    add ex091

---
+++ ex091/Program.cs
@@ -0,0 +1,22 @@
+﻿// Замена элемнта массива
+//Максимум элемента массива с помоцью функции (метод)
+
+int Max(int ar1, int ar2, int ar3)
+{
+    int result = ar1;
+    if (ar2 > ar1) result = ar2;
+    if (ar3 > ar2) result = ar3;
+    return result;
+}
+
+int[] array = {1,2,3,4,5,16,7,8,9};
+
+array[0]=12;
+Console.WriteLine(array[0]);
+
+int result = Max(
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8])
+);
+Console.WriteLine($"{result} Сумма элементов массива");
