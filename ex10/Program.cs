b9eabcec0332e7b982e32e3645550e14e86c7c5d
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-03 14:30:40 +0300

    add ex10

---
+++ ex10/Program.cs
@@ -0,0 +1,27 @@
+﻿void Fillarray(int[] argCol)
+{
+    int len = argCol.Length;
+    int index = 0;
+    while (index < len)
+    {
+        argCol[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void printArray(int[] argArr)
+{
+    int N = argArr.Length;
+    int i = 0;
+    while(i<N)
+    {
+        Console.WriteLine(argArr[i]);
+        i++;
+    }
+
+}
+
+int[] array = new int[10];
+ 
+ Fillarray(array);
+ printArray(array);
